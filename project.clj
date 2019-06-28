36fc62fa91f4df5a3f122019d43b4ad598137dcc
Diego <diego.vid.eco@gmail.com>
2019-06-27 23:00:03 -0500

    wip variante de la implementación del converge

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [overtone "0.10.3"]
-                 [cider/cider-nrepl "0.21.1"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [overtone "0.10.3"]])
--- src/nanc_in_a_can/core.clj
+++ src/nanc_in_a_can/core.clj
@@ -1,37 +1,37 @@
 (ns nanc-in-a-can.core
   (:use [overtone.live]))
 
-(comment (
-SynthDef(\sin, {|
-	freq = 400,
-	portamento = 0,
-	portamento_amp = 0.3,
-	portamento_amp_speed = 0.3,
-	amp = 0.1,
-	gate = 1,
-	out = 0
-	|
-	var sig = {
-		SinOsc.ar(
-			Lag.kr(freq, portamento),
-			0,
-			Lag.kr(portamento_amp, portamento_amp_speed)
-		)
-		.pipe(
-			// BPeakEQ.ar(_, [800, 1000, 2000, 4000], 1, -40),
-			BHiShelf.ar(_, freq: 800, rs: 0.3, db: -10),
-			_* EnvGen.kr(
-				Env.asr(0.1, amp, 1.3),
-				gate,
-				doneAction:2
-			),
-			_*amp*0.1,
-			Limiter.ar(_, level: 0.5, dur: 0.03)
-		)
-	} ;
-	Out.ar(out, sig ! 2);
-}).add;
-))
+;; (comment (
+;; SynthDef(\sin, {|
+;; 	freq = 400,
+;; 	portamento = 0,
+;; 	portamento_amp = 0.3,
+;; 	portamento_amp_speed = 0.3,
+;; 	amp = 0.1,
+;; 	gate = 1,
+;; 	out = 0
+;; 	|
+;; 	var sig = {
+;; 		SinOsc.ar(
+;; 			Lag.kr(freq, portamento),
+;; 			0,
+;; 			Lag.kr(portamento_amp, portamento_amp_speed)
+;; 		)
+;; 		.pipe(
+;; 			// BPeakEQ.ar(_, [800, 1000, 2000, 4000], 1, -40),
+;; 			BHiShelf.ar(_, freq: 800, rs: 0.3, db: -10),
+;; 			_* EnvGen.kr(
+;; 				Env.asr(0.1, amp, 1.3),
+;; 				gate,
+;; 				doneAction:2
+;; 			),
+;; 			_*amp*0.1,
+;; 			Limiter.ar(_, level: 0.5, dur: 0.03)
+;; 		)
+;; 	} ;
+;; 	Out.ar(out, sig ! 2);
+;; }).add;
+;; ))
 
 
 (defsynth sin1 [freq 400 
@@ -53,13 +53,14 @@ SynthDef(\sin, {|
 (stop)
 
 
+;; nucleo del converge original traducido a clj
 (defn make-bcp [cp line] (sum (subvec line 0 (max 0 (- cp 1)))))
 
 (defn make-tempo [speed] (/ 60 (/ speed 4)))
 
 (defn total-dur [voice] (sum (map :dur (:melody voice))))
 
-(defn make-voices [cp voices melody]
+(defn make-voices [{:keys [cp voices melody]}]
   (->> voices 
        (map (fn [voice] ; define each melody event for each voice 
               (->> melody 
@@ -76,16 +77,57 @@ SynthDef(\sin, {|
           (let [min-bcp  (apply min (map #(get-in % [:bcp]) voices))]
             (map #(assoc % :onset (Math/abs (float (- min-bcp (:bcp %))))) voices))))))
 
-(make-voices
- 2
- [{:tempo 60 :transp 0} {:tempo 50 :transp 7}]
- [{:dur 1 :note 60} {:dur 2 :note 62}])
+(user/spy (make-voices
+           {:cp 2
+            :voices [{:tempo 60 :transp 0} {:tempo 50 :transp 7}]
+            :melody [{:dur 1 :note 60} {:dur 2 :note 62}]}))
 
 (defn converge [symbol melody cp voices instruments player repeat osc meta] melody)
 
 (make-bcp 3 [0 1 2 3 4])
 
 
-(make-bcp 4 [3 6])
-(apply min [1 2 3])
+;; (make-bcp 4 [3 6])
+;; (apply min [1 2 3])
 
+
+;; wip implementando converge de otra manera, con :elapsed en durs
+(do
+  (defn converge [{:keys [durs tempos]}]
+    (let [vdurs (map (fn [tempo] 
+                       (reduce (fn [acc dur]
+                                 (let [new-dur (/ dur tempo)
+                                       last-dur (-> acc last :dur (or 0))
+                                       last-elapsed (-> acc last :elapsed (or 0))
+                                       elapsed (+ last-dur last-elapsed)]
+                                   (conj acc {:dur new-dur :elapsed elapsed}))
+                                 ) [] durs)) 
+                     tempos)
+          ]
+      vdurs)
+    )
+
+  ((user/capture :vdurs) (converge {:durs [1 2 3 4]
+                               :tempos [1 2 3]})))
+
+(-> []  last :elapsed (or 0))
+
+
+;; transposicion temporal usando :elapsed, multiples cps
+(let [vdurs (@user/data :vdurs)
+      sorted-durs (->> vdurs (sort-by (comp - :elapsed last))) ;; from longest to shortest
+      longest (first sorted-durs)
+      cp [2 3]
+      get-elapsed-at-cp (fn [cp voice] (-> voice (nth cp) :elapsed))
+      cps-at-longest (mapv #(get-elapsed-at-cp % longest) cp)
+      add-cp-offset (fn [index voice] 
+                      (let [current-cp (cp (mod index (count cps-at-longest)))
+                            current-elapsed-cp (cps-at-longest (mod index (count cps-at-longest)))
+                            offset (- current-elapsed-cp (get-elapsed-at-cp current-cp voice))]
+                        (map 
+                         (fn [vdur] 
+                           (update-in vdur [:elapsed] #(+ offset %)))
+                         (user/spy :mute :voice voice))))]
+  (user/spy :longest longest)
+  (user/spy :shorter (map-indexed add-cp-offset (rest vdurs)))
+  )
