01d3dd1f42997991539aeb76727584228c4be60c
Alejandro <afrancob17@gmail.com>
2019-10-20 19:34:56 -0400

    Update sequencing-2.clj

--- src/nanc_in_a_can/sequencing-2.clj
+++ src/nanc_in_a_can/sequencing-2.clj
@@ -1,11 +1,11 @@
-(ns sequencing-2
+(ns nanc-in-a-can.sequencing-2
   (:require [overtone.music.time :refer [apply-at now]]))
 
-(def config (atom {:lag 1000}))
-(def voice (atom {:fn #(println "hola")
+(def config (atom {:lag 100}))
+(def voice (atom {:fn #(println "adios")
                   :started-at 1227874823742348
                   :next-event (+ 1000 (now))
-                  :durs [1 1/3 1/3 1/3]
+                  :durs [1 1/4 1/4 1/4 1/4]
                   :index 0
                   :ratio 1
                   :tempo 2000
@@ -13,37 +13,37 @@
                   :playing? true}))
 (do
   (swap! voice #(merge % {:loop? true :next-event (+ 1000 (now))}))
-  (declare schedule)
-  (defn update-voice-and-schedule-next-event 
-    [voice]
-    (let [{:keys [durs index tempo loop?] :as v} @voice
-          next-index(inc index)
-          finished? (>= next-index (count durs))
-          reset? (user/spy (and finished? loop?))
-          next-index* (if-not reset? next-index 0)
-          next-event (+ (* tempo (durs next-index*)) 
-                        (:next-event v))
-          schedule? (or loop? (not finished?))
-          ]
-      (swap! voice #(merge % {:index (user/spy :index next-index*)
-                              :next-event next-event}))
-      (when schedule? (schedule voice))
-        
-      ))
 
+  (defn calculate-next-voice-state
+    "Calculates next state of a voice and if it should be scheduled"
+    [voice]
+    (let [{:keys [durs index tempo loop?]} voice
+          next-index (inc index)
+          finished? (>= next-index (count durs))
+          reset? (and finished? loop?)
+          next-index* (if-not reset? next-index 0)
+          next-event (+ (* tempo (nth durs next-index* index))
+                        (:next-event voice))
+          schedule? (or loop? (not finished?))]
+      {:voice (merge voice {:index next-index*
+                            :next-event next-event})
+       :schedule? schedule?}))
+  
   (defn schedule [voice]
     (let [{:keys [next-event fn]} @voice
-          fn* 
-          #(when (:playing? @voice) 
-             (do 
+          fn*
+          #(when (:playing? @voice)
+             (do
                (fn)
-               (update-voice-and-schedule-next-event voice)))
+               (let [{schedule? :schedule? voice* :voice} (calculate-next-voice-state
+                                                           @voice)]
+                 (reset! voice voice*)
+                 (when schedule? (schedule voice)))))
           event #(do
-                   (println "scheduleando")          
+                   (println "scheduleando")
                    (apply-at next-event fn*))]
       (apply-at (- next-event (:lag @config)) event)))
 
   (schedule voice))
 
 (swap! voice #(assoc % :loop? false))
-
