87b16d44c8472d0c36e3c982d7dd5a3d030c8afa
Diego <diego.vid.eco@gmail.com>
2019-11-16 15:45:13 -0600

    Generalizing test

--- src/nanc_in_a_can/dynacan.clj
+++ src/nanc_in_a_can/dynacan.clj
@@ -28,21 +28,27 @@
      :elapsed (* ratio (+ elapsed-in-completed last-elapsed))}))
 
 ;;;; given a list of durs, a ratio, an event at index cp and a moment in time calculate most inmediate possible event (the one closest to elapsed-at >= 0
-(do
-  (defn get-partial-cycle-durs
-    "start-index must already be in modulo form"
-    [dur-acc ratio durs start-index max-elapsed]
-    (loop [dur-acc* dur-acc
-           i (dec start-index)]
-      (let [next-dur (* ratio (nth durs i 0))
-            next-acc-val (+ dur-acc* next-dur)]
-        (if (or (< i 0)
-                (> next-acc-val max-elapsed))
-          {:index (inc i) :elapsed dur-acc* :elapsed-at (- max-elapsed dur-acc*)}
-          (recur next-acc-val (dec i))))))
+(do)
+(defn get-partial-cycle-durs
+  "start-index must already be in modulo form"
+  [dur-acc ratio durs start-index max-elapsed]
+  (user/spy :duracc dur-acc)
+  (loop [dur-acc* dur-acc
+         i (dec start-index)]
+    (user/spy :durac1 dur-acc*)
+    (let [next-dur (* ratio (nth durs i 0))
+          next-acc-val (+ dur-acc* next-dur)]
+      (if (or (< i 0)
+              (> next-acc-val max-elapsed))
+        (do (user/spy :durac dur-acc*)
+            {:index (inc i)
+             :elapsed dur-acc*
+             :elapsed-at (user/spy :ea max-elapsed dur-acc* (- max-elapsed dur-acc*))
+             :completed-all (> next-acc-val max-elapsed) })
+        (recur next-acc-val (dec i))))))
 
-  (defn find-first-event-using-cp
-    "The first event can be deduced by conceptualizing that the first event may be found by going
+(defn find-first-event-using-cp
+  "The first event can be deduced by conceptualizing that the first event may be found by going
   backwards in time starting from the cp.
   Th cp is at some point of the durs vector (cycle) so we need to start from there and go back to the begining of the vector.
   First comes a partial cycle (pcy-end), which may be from 0 to the nth index of the nth length durs vector.
@@ -54,61 +60,72 @@
   The difference of cp-elapsed-at with the previous result can not be smaller than 0.
   The value that is returned is the dur index and the elapsed-at of the first event.
   "
-    [ratio durs cp cp-elapsed-at]
-    (let [durs-size (count durs)
-          pcy-end (user/spy :pcy-end (get-partial-cycle-durs 0 ratio durs (mod cp durs-size) cp-elapsed-at))
-          cycle-total (user/spy :cycle-total (* ratio (apply + durs)))
-          cyn->cy0 (user/spy (quot (pcy-end :elapsed-at) cycle-total))
-          pcy-end+cyn->cy0 (+ (pcy-end :elapsed) (* cyn->cy0 cycle-total))
-          pcy-start (get-partial-cycle-durs pcy-end+cyn->cy0
-                                            ratio
-                                            durs
-                                            durs-size
-                                            cp-elapsed-at)
-          index (mod (pcy-start :index) durs-size)]
-      {:elapsed-at (pcy-start :elapsed-at)
-       :index index
-       :real-index (cond
-                     ;; if no full cycles will elapse, transpose index to the correct cycle of given by `cp-elapsed-at`
-                     (= 0 cyn->cy0) (-> (quot cp durs-size)
-                                        (* durs-size)
-                                        (+ index))
-                     ;; if many full cycles, at which index should it really start (negative index) so that the indexes of both voices coincide at the same `elapsed-at`
-                     (> cyn->cy0 0) (-> cyn->cy0
-                                        (* durs-size)
-                                        (+ (:index pcy-end))
-                                        -
-                                        (+ cp))
-                     :default index)}
-      ))
-  (find-first-event-using-cp 8/7 durs 7 7))
+  [ratio durs cp cp-elapsed-at]
+  (let [durs-size (count durs)
+        pcy-end (user/spy :pcy-end (get-partial-cycle-durs 0 ratio durs (mod cp durs-size) cp-elapsed-at))
+        cycle-total (user/spy :mute :cycle-total (* ratio (apply + durs)))
+        cyn->cy0 (user/spy :mute (quot (pcy-end :elapsed-at) cycle-total))
+        pcy-end+cyn->cy0 (+ (pcy-end :elapsed) (* cyn->cy0 cycle-total))
+        pcy-start (user/spy :start (get-partial-cycle-durs
+                                    (user/spy :pcy-end-acc pcy-end+cyn->cy0)
+                                    ratio
+                                    durs
+                                    durs-size
+                                    cp-elapsed-at
+                                       ))
+        index (mod (pcy-start :index) durs-size)]
+    {:elapsed-at (if (pcy-end :completed-all)
+                   (pcy-end :elapsed-at)
+                   (pcy-start :elapsed-at))
+     :index index
+     ;; :real-index (cond
+     ;;               ;; if no full cycles will elapse, transpose index to the correct cycle of given by `cp-elapsed-at`
+     ;;               (= 0 cyn->cy0) (-> (quot cp durs-size)
+     ;;                                  (* durs-size)
+     ;;                                  (+ index))
+     ;;               ;; if many full cycles, at which index should it really start (negative index) so that the indexes of both voices coincide at the same `elapsed-at`
+     ;;               (> cyn->cy0 0) (-> cyn->cy0
+     ;;                                  (* durs-size)
+     ;;                                  (+ (:index pcy-end))
+     ;;                                  -
+     ;;                                  (+ cp))
+     ;;               :default index)
+     }))
+  ;; (find-first-event-using-cp 8/7 durs 7 7)
+  
 
 
-(defn get-next-event [durs voice]
-  {:index (inc (:index @voice))
-   :elapsed-at (+ (:elapsed-at @voice)
-                  (nth durs (mod (:index @voice) (count durs))))})
+(defn get-next-event [voice durs]
+  (merge voice {:index (inc (:index voice))
+                :dur (* (voice :ratio)
+                        (nth durs (mod (voice :index) (count durs))))
+                :elapsed (+ (get voice :dur 0)
+                            (get voice
+                                 :elapsed
+                                 (:elapsed-at voice)))}))
 
-                                        ;(swap! v1' #(merge % (get-next-event durs v1')))
+
 
 
 ;;;;
 
 (defn get-next-n-events [durs voice n]
   (loop [n* n
-         index (:index @voice)
+         index (:index voice)
          res []]
     (if (= -1 n*)
       res
       (recur (dec n*)
              (inc index)
-             (conj res {:index index
-                        :dur (* (@voice :ratio)
-                                (nth durs (mod index (count durs))))
-                        :elapsed (+ (get (last res) :dur 0)
-                                    (get (last res)
-                                         :elapsed
-                                         (:elapsed-at @voice)))})))))
+             (conj res
+                   (merge voice
+                          {:index index
+                           :dur (* (voice :ratio)
+                                   (nth durs (mod index (count durs))))
+                           :elapsed (+ (get (last res) :dur 0)
+                                       (get (last res)
+                                            :elapsed
+                                            (:elapsed-at voice)))}))))))
 
 (comment
   ;;test
@@ -120,56 +137,6 @@
 ;;Test;;
 ;;;;;;;;
 
-;; Demonstrate that the event at `cp` in two different voices occurs at the same time
-;; each voices is a map in an atom with keys: `:ratio` `:elapsed-at` `:index`
-;; `elapsed-at` and `:index` for both voices are calculated with
-;; `find-first-event-using-cp` and `get-event-at`.
-;; IMPORTANT: Note that `get-event-at` always uses the `reference-ratio`.
-;; This should be the same for all voices in the canon.
-
-;; The function `get-next-n-events` is used as means for the demonstration,
-;; see comment with example use.
-
-;; To Do: convert into a proper test
-(comment
-  (do
-    (def cp 7)
-    (def durs [1 1])
-    (def reference-ratio 1)
-    (def subordinate-ratio 8/7)
-    (def elapsed-time-at-cp (:elapsed
-                             (get-event-at reference-ratio
-                                           durs
-                                           cp)))
-    (def v1' (atom (merge {:ratio reference-ratio}
-                          (find-first-event-using-cp reference-ratio
-                                                     durs
-                                                     cp
-                                                     elapsed-time-at-cp))))
-
-    (def v2' (atom (merge {:ratio subordinate-ratio}
-                          (find-first-event-using-cp subordinate-ratio
-                                                     durs
-                                                     cp
-                                                     elapsed-time-at-cp)
-                                        ; {:index -14}
-                          )))
-    ;; just print both voices
-    (println "Voices data:")
-    (println @v1')
-    (println @v2')
-
-    (->> [v1' v2']
-         (map #(as-> % v
-                 (get-next-n-events durs v 21) ;; generate 7 events
-                 (user/spy v)
-                 (filterv (fn [ev] (= 7 (:index ev))) v)
-                 (map :elapsed v) ;; get elapsed value
-                 ))
-         flatten
-         (user/spy "..........Elapsed at times..........")
-         (apply =)  ;; verify that both voices occur at the same time
-         (user/spy "=======Voices are equal?========="))))
 
 
 
@@ -218,3 +185,64 @@
 
   (swap! v1' #(assoc % :loop? false))
   (swap! v2' #(assoc % :loop? false)))
+
+
+
+
+;; Demonstrate that the event at `cp` in two different voices occurs at the same time
+;; each voices is a map in an atom with keys: `:ratio` `:elapsed-at` `:index`
+;; `elapsed-at` and `:index` for both voices are calculated with
+;; `find-first-event-using-cp` and `get-event-at`.
+;; IMPORTANT: Note that `get-event-at` always uses the `reference-ratio`.
+;; This should be the same for all voices in the canon.
+
+;; The function `get-next-n-events` is used as means for the demonstration,
+;; see comment with example use.
+
+;; To Do: convert into a proper test
+
+
+(comment)
+(defn normalize-dur [{:keys [dur ratio]}] (* dur (/ 1 ratio)))
+
+(do
+  (def cp 11)
+  (def durs [1 10 1/2])
+  (def reference-ratio 1)
+  (def subordinate-ratio 3/11)
+  (def elapsed-time-at-cp (:elapsed
+                           (get-event-at reference-ratio
+                                         durs
+                                         cp)))
+  (def main-voice (merge {:ratio reference-ratio}
+                         (find-first-event-using-cp reference-ratio
+                                                    durs
+                                                    cp
+                                                    elapsed-time-at-cp)))
+
+  (def secondary-voice (merge {:ratio subordinate-ratio}
+                              (find-first-event-using-cp subordinate-ratio
+                                                         durs
+                                                         cp
+                                                         elapsed-time-at-cp)))
+  
+  ;; just print both voices
+  (println "Voices data:")
+  (println main-voice)
+  (println secondary-voice)
+
+  (let [mv-event (last (get-next-n-events durs main-voice cp))
+        mv-elapsed-at-cp (:elapsed mv-event)]
+    (loop [sv-event (get-next-event secondary-voice durs)]
+      (let [sv-elapsed (get sv-event :elapsed 0)
+            both-same-duration (= (normalize-dur mv-event)
+                                  (normalize-dur sv-event))]
+        (cond
+          (and (= mv-elapsed-at-cp sv-elapsed)
+               both-same-duration) (user/spy [true mv-event sv-event :same-duration])
+          (= mv-elapsed-at-cp sv-elapsed) (user/spy [false mv-event sv-event :different-duration])
+          (< mv-elapsed-at-cp sv-elapsed) [false mv-event sv-event]
+          :else (recur (get-next-event sv-event durs))))))
+)
+  
+
