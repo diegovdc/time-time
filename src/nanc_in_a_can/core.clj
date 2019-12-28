2887e841b8159967e3e3be8b801937fd1f588fa6
Diego <diego.vid.eco@gmail.com>
2019-12-27 20:56:54 -0600

    Adds period to canon

--- src/nanc_in_a_can/core.clj
+++ src/nanc_in_a_can/core.clj
@@ -1,157 +1,162 @@
 (ns nanc-in-a-can.core
-  (:use [overtone.live]))
-
-;; (comment (
-;; SynthDef(\sin, {|
-;; 	freq = 400,
-;; 	portamento = 0,
-;; 	portamento_amp = 0.3,
-;; 	portamento_amp_speed = 0.3,
-;; 	amp = 0.1,
-;; 	gate = 1,
-;; 	out = 0
-;; 	|
-;; 	var sig = {
-;; 		SinOsc.ar(
-;; 			Lag.kr(freq, portamento),
-;; 			0,
-;; 			Lag.kr(portamento_amp, portamento_amp_speed)
-;; 		)
-;; 		.pipe(
-;; 			// BPeakEQ.ar(_, [800, 1000, 2000, 4000], 1, -40),
-;; 			BHiShelf.ar(_, freq: 800, rs: 0.3, db: -10),
-;; 			_* EnvGen.kr(
-;; 				Env.asr(0.1, amp, 1.3),
-;; 				gate,
-;; 				doneAction:2
-;; 			),
-;; 			_*amp*0.1,
-;; 			Limiter.ar(_, level: 0.5, dur: 0.03)
-;; 		)
-;; 	} ;
-;; 	Out.ar(out, sig ! 2);
-;; }).add;
-;; ))
-
-
-(defsynth sin1 [freq 400 
-               portamento 0 
-               portamento-amp 0.3
-               portamento-amp-speed 0.3 
-               amp 0.1 
-               gate 1 
-               out' 0]
-  (let [sig (sin-osc 
-             (lag:kr freq portamento) 
-             0 
-             (lag:kr portamento-amp portamento-amp-speed))
-        sig (b-hi-shelf sig 800 0.3 -10)
-        sig (* sig amp 0.1)]
-    (out out' sig)))
-
-(sin1 440)
-(stop)
-
-
-;; nucleo del converge original traducido a clj
-(defn make-bcp [cp line] (sum (subvec line 0 (max 0 (- cp 1)))))
-
-(defn make-tempo [speed] (/ 60 (/ speed 4)))
-
-(defn total-dur [voice] (sum (map :dur (:melody voice))))
-
-(defn make-voices [{:keys [cp voices melody]}]
-  (->> voices 
-       (map (fn [voice] ; define each melody event for each voice 
-              (->> melody 
-                   (map (fn [event] 
-                          {:dur (* (:dur event) (make-tempo (:tempo voice)))
-                           :note (+ (:note event) (:transp voice))
-                           :amp (:amp event)
-                           :cp cp})))))
-       (map (fn [voice] ; add bcp (before convergence point)
-              (let [bcp  (make-bcp cp (mapv :dur voice))] 
-                {:melody voice :bcp bcp})))
-       (sort (fn [a, b] (> (total-dur a) (total-dur b)))); sort voices by duration
-       ((fn [voices] ; add onset data
-          (let [min-bcp  (apply min (map #(get-in % [:bcp]) voices))]
-            (map #(assoc % :onset (Math/abs (float (- min-bcp (:bcp %))))) voices))))))
-
-(user/spy (make-voices
-           {:cp 2
-            :voices [{:tempo 60 :transp 0} {:tempo 50 :transp 7}]
-            :melody [{:dur 1 :note 60} {:dur 2 :note 62}]}))
-
-(defn converge [symbol melody cp voices instruments player repeat osc meta] melody)
-
-(make-bcp 3 [0 1 2 3 4])
-
-
-;; (make-bcp 4 [3 6])
-;; (apply min [1 2 3])
-
-
-;; wip implementando converge de otra manera, con :elapsed en durs
-(do
-  (defn converge [{:keys [durs tempos]}]
-    (let [vdurs (map (fn [tempo] 
-                       (reduce (fn [acc dur]
-                                 (let [new-dur (/ dur tempo)
-                                       last-dur (-> acc last :dur (or 0))
-                                       last-elapsed (-> acc last :elapsed (or 0))
-                                       elapsed (+ last-dur last-elapsed)]
-                                   (conj acc {:dur new-dur :elapsed elapsed}))
-                                 ) [] durs)) 
-                     tempos)
-          ]
-      vdurs)
-    )
-
-  ((user/capture :vdurs) (converge {:durs [1 2 3 4]
-                               :tempos [1 2 3]})))
-
-(-> []  last :elapsed (or 0))
-
+  (:require [nanc-in-a-can.standard :refer [dur->sec]]))
 
 ;; transposicion temporal usando :elapsed, multiples cps
-((user/capture :canon)
- (let [vdurs (@user/data :vdurs)
-       sorted-durs (->> vdurs (sort-by (comp - :elapsed last))) ;; from longest to shortest
-       longest (first sorted-durs)
-       total-dur (->> (last longest) vals (apply +))
-       cp [2 3]
-       get-elapsed-at-cp (fn [cp voice] (-> voice (nth cp) :elapsed))
-       cps-at-longest (mapv #(get-elapsed-at-cp % longest) cp)
-       add-cp-offset (fn [index voice] 
-                       (let [current-cp (cp (mod index (count cps-at-longest)))
-                             current-elapsed-cp (cps-at-longest (mod index (count cps-at-longest)))
-                             offset (- current-elapsed-cp (get-elapsed-at-cp current-cp voice))]
-                         (mapv 
-                          (fn [vdur] 
-                            (update-in vdur [:elapsed] #(+ offset %)))
-                          (user/spy :mute :voice voice))))
-       add-remainder (fn [voice] (let [remainder (->> (last voice)
-                                                     vals
-                                                     (apply +)
-                                                     (- total-dur))
-                                      last-event {:dur remainder  
-                                                  :elapsed (+ (:dur (last voice)) (:elapsed (last voice))) 
-                                                  :remainder? true}]
-                                  (conj voice last-event)))
-       offseted-voices (map-indexed add-cp-offset (rest vdurs))]
-   (map add-remainder (conj offseted-voices longest))
-   ;; (user/spy :longest longest)
-   ;; (user/spy :shorter (map-indexed add-cp-offset (rest vdurs)))
-   ))
+(defn- converge-transposition [durs cps]
+  (let [vdurs durs
+        cp (if (vector? cps) cps [cps])
+        sorted-durs (->> vdurs (sort-by (comp - :elapsed last))) ;; from longest to shortest
+        longest (first sorted-durs)
+        total-dur (->> (last longest) vals (apply +))
+        get-elapsed-at-cp (fn [cp voice] (-> voice (nth cp) :elapsed))
+        cps-at-longest (mapv #(get-elapsed-at-cp % longest) cp)
+        add-cp-offset (fn [index voice]
+                        (let [current-cp (cp (mod index (count cps-at-longest)))
+                              current-elapsed-cp (cps-at-longest
+                                                  (mod index
+                                                       (count cps-at-longest)))
+                              offset (- current-elapsed-cp
+                                        (get-elapsed-at-cp current-cp voice))]
+                          (mapv
+                           (fn [vdur]
+                             (update-in vdur [:elapsed] #(+ offset %)))
+                           (user/spy :mute :voice voice))))
+        add-remainder (fn [voice] (let [remainder (->> (last voice)
+                                                       vals
+                                                       (apply +)
+                                                       (- total-dur))
+                                        last-event {:dur remainder
+                                                    :elapsed (+ (:dur (last voice))
+                                                                (:elapsed (last voice)))
+                                                    :remainder? true}]
+                                    (conj voice last-event)))
+        offseted-voices (map-indexed add-cp-offset (rest vdurs))]
+    (map add-remainder (conj offseted-voices longest))))
+
+(defn get-period-scaling-factor
+  "Returns a factor to rescale tempos to fit make a canon fit a given period"
+  [period total-dur bpm]
+  (->> (dur->sec total-dur bpm)
+       (/ period)))
+
+(defn converge [{:keys [durs tempos cps period bpm] :or {cps [0]}}]
+  "Optional params:
+    :period - in seconds
+    :bpm - used for setting period, and required because of that"
+  (let [slowest-tempo (apply min tempos)
+        total-dur (->> durs
+                       (map #(* slowest-tempo %))
+                       (apply +))
+        scaling-factor (if-not (and period bpm)
+                         1
+                         (get-period-scaling-factor
+                          period
+                          total-dur
+                          bpm))
+        vdurs (map-indexed
+               (fn [index tempo]
+                 (reduce (fn [acc dur]
+                           (let [new-dur (* scaling-factor (/ dur tempo))
+                                 last-dur (-> acc last :dur (or 0))
+                                 last-elapsed (-> acc last :elapsed (or 0))
+                                 elapsed (+ last-dur last-elapsed)]
+                             (conj acc
+                                   {:dur new-dur
+                                    :elapsed elapsed
+                                    :tempo tempo
+                                    :tempo-index index})))
+                         []
+                         durs))
+               tempos)
+        canon (converge-transposition vdurs cps)]
+    (with-meta canon {:total-dur total-dur
+                      :bpm (and period bpm)
+                      :period (and bpm period)
+                      :scaled-dur (* total-dur scaling-factor)
+                      :scaled? (not= 1 scaling-factor)
+                      :scaling-factor scaling-factor})))
+
+
+(defn canon-dur
+  "Returns the total duration of the canon in seconds"
+  [canon bpm]
+  (-> canon meta :total-dur (dur->sec bpm)))
+
+(do
+  ;;;  test `get-period-scaling-factor`
+  (let [durs [1 1 1 1 1]
+        slowest-tempo 1
+        total-dur (* slowest-tempo (apply + durs))
+        period 3
+        scaling-factor (get-period-scaling-factor period total-dur 60)]
+    (->> durs (map #(* % scaling-factor))
+         (apply +)
+         (= period))))
+
+(comment
+  (def canon (converge {:durs (flatten (repeat 300 [1/4 1]))
+                        :tempos [5 4 2 1]
+                        :cps [150]}))
+  (def canon-2 (converge {:durs (repeat 5 1)
+                          :tempos [1 2]
+                          :cps [3]}))
+  (def canon-3 (converge {:durs (repeat 5 1)
+                          :tempos [1 2]
+                          :cps [3]
+                          :period 3
+                          :bpm 60}))
+  (def canon-4 (converge {:durs (repeat 5 1)
+                          :tempos [1 2]
+                          :cps [3]
+                          :period 3
+                          :bpm 120}))
+  (meta canon-2)
+  (meta canon-3)
+  (meta canon-4)
+  (canon-3)
+  (canon-dur canon-2 60))
+
 
 (comment ;; test that all voices have a remainder and their duration is the same
-  (->> @user/data :canon user/spy 
+  (->> @user/data :canon user/spy
        (map (comp #(apply + %)
-                  (fn [voice] (filter #(not= true %) voice)) 
-                  vals  
+                  (fn [voice] (filter #(not= true %) voice))
+                  vals
                   last))
-       (apply =)
-       ))
+       (apply =)))
 
 
 
+(comment
+  ;; nucleo del converge original traducido a clj
+  (defn make-bcp [cp line] (sum (subvec line 0 (max 0 (- cp 1)))))
+
+  (defn make-tempo [speed] (/ 60 (/ speed 4)))
+
+  (defn total-dur [voice] (sum (map :dur (:melody voice))))
+
+  (defn make-voices [{:keys [cp voices melody]}]
+    (->> voices
+         (map (fn [voice] ; define each melody event for each voice
+                (->> melody
+                     (map (fn [event]
+                            {:dur (* (:dur event) (make-tempo (:tempo voice)))
+                             :note (+ (:note event) (:transp voice))
+                             :amp (:amp event)
+                             :cp cp})))))
+         (map (fn [voice] ; add bcp (before convergence point)
+                (let [bcp  (make-bcp cp (mapv :dur voice))]
+                  {:melody voice :bcp bcp})))
+         (sort (fn [a, b] (> (total-dur a) (total-dur b)))); sort voices by duration
+         ((fn [voices] ; add onset data
+            (let [min-bcp  (apply min (map #(get-in % [:bcp]) voices))]
+              (map #(assoc % :onset (Math/abs (float (- min-bcp (:bcp %))))) voices))))))
+
+  (user/spy (make-voices
+             {:cp 2
+              :voices [{:tempo 60 :transp 0} {:tempo 50 :transp 7}]
+              :melody [{:dur 1 :note 60} {:dur 2 :note 62}]}))
+
+  (defn converge [symbol melody cp voices instruments player repeat osc meta] melody)
+
+  (make-bcp 3 [0 1 2 3 4]))
--- src/nanc_in_a_can/sequencing.clj
+++ src/nanc_in_a_can/sequencing.clj
@@ -10,7 +10,7 @@
 
 (stop)
 (kick)
- (grey-whale)
+(grey-whale)
 (def nome (metronome 200))
 (definst kick* [] (pan2 (play-buf:ar 1 kick)))
 (definst whales-1* [pan 0] (pan2 (play-buf:ar 1 whales-1) pan))
@@ -27,87 +27,79 @@
 (defn sequencer- [nome sequence* on-event state]
   (let [{:keys [start-at index repeat]} @state
         val* (first sequence*)]
-    (at (nome (+ start-at (:elapsed val*))) 
+    (at (nome (+ start-at (:elapsed val*)))
         (on-event val* index)
         (if-not (:remainder? (user/spy val*))
-          (apply-by (nome (+ start-at (:elapsed (second sequence*)))) 
-                    sequencer- 
+          (apply-by (nome (+ start-at (:elapsed (second sequence*))))
+                    sequencer-
                     nome
                     (rest sequence*)
-                    on-event 
+                    on-event
                     [(do (swap! state #(update-in % [:index] inc))
                          state)])
           (if (not= 0 repeat)
-            (apply-by (nome (+ start-at (:elapsed val*) (:dur val*))) 
-                      sequencer- 
+            (apply-by (nome (+ start-at (:elapsed val*) (:dur val*)))
+                      sequencer-
                       nome
                       (@state :sequence)
-                      on-event 
-                      [(do (swap! state 
+                      on-event
+                      [(do (swap! state
                                   #(-> %
-                                       (assoc :start-at 
+                                       (assoc :start-at
                                               (+ start-at (:elapsed val*) (:dur val*)))
-                                       (update-in [:index] 
+                                       (update-in [:index]
                                                   (constantly 0))
-                                       (update-in [:repeat] 
+                                       (update-in [:repeat]
                                                   (fn [r] (if (= r :inf) r (dec r))))))
                            state)])))))
   state)
-  
+
 (defn sequencer
-  ([nome sequence on-event] (sequencer- nome sequence on-event (atom {:on-event on-event
-                                                                      :sequence sequence
-                                                                      :start-at (nome)
-                                                                      :index 0})))
-  ([nome sequence on-event {:keys [repeat offset] :or {offset 0} :as opts
-                            }] 
-   (sequencer- nome sequence on-event (atom (merge opts 
-                                                   {:on-event on-event
-                                                    :sequence sequence
-                                                    :start-at (+ (nome) offset)
-                                                    :index 0})))))
-(comment  
+  ([nome sequence on-event]
+   (sequencer- nome
+               sequence
+               on-event
+               (atom {:on-event on-event
+                      :sequence sequence
+                      :start-at (nome)
+                      :index 0})))
+  ([nome sequence on-event {:keys [repeat offset]
+                            :or {offset 0}
+                            :as opts}]
+   (sequencer- nome
+               sequence
+               on-event
+               (atom (merge opts
+                            {:on-event on-event
+                             :sequence sequence
+                             :start-at (+ (nome) offset)
+                             :index 0})))))
+(comment
   (let [nome* (metronome 60)
         now- (now)]
     ((user/capture :sequencer) (sequencer
-                                nome 
-                                (conj (mapv #(hash-map :elapsed %) (range 5)) 
-                                      {:remainder? true :dur 1 :elapsed 5})  
+                                nome
+                                (conj (mapv #(hash-map :elapsed %) (range 5))
+                                      {:remainder? true :dur 1 :elapsed 5})
                                 (fn [vals index]
                                         ;(swap! quil-test/st #(assoc % :bg (rand 255)))
-                                  (hh) 
-                                  nil) 
-                                {:repeat :inf}
-                                ))))
- 
+                                  (hh)
+                                  nil)
+                                {:repeat :inf}))))
 
 (comment
-  (-> @user/data :sequencer (swap! #(update-in % [:repeat] (constantly nil))))
-  (stop)
-  (kill-server))
-
-(comment (overtone.at-at/stop-and-reset-pool! my-pool))
-
-(comment (overtone.sc.server/boot-server))
-
-
-(comment
-  (->> @user/data :canon
-       (map (fn [voice] (sequencer 
-                        nome 
-                        voice
-                        (fn [vals index]
-                          (if (= 0 (mod index 2))
-                            (swap! quil-test/st #(assoc % :bg (rand 255)))
-                            (swap! quil-test/st #(assoc % :radius (rand 3.0)))
-                            )
-                          ((rand-nth [kick hh])) 
-                          nil) 
-                        {:repeat 10}
-                        )))))
-
-
- 
-(comment                                      
-  (recording-start "~/Desktop/foo.wav")
-  (recording-stop))
+  (require '[nanc-in-a-can.core :refer [converge]])
+  (def kick (freesound 2086))
+  (let [nome (metronome 120)]
+    (->> (converge {:durs (repeat 6 1)
+                    :tempos [1]
+                    :cps [5]
+                    :bpm 120
+                    :period 1})
+         (map (fn [voice] (sequencer
+                           nome
+                           voice
+                           (fn [vals index]
+                             (kick)
+                             nil)
+                           {:repeat nil}))))))
