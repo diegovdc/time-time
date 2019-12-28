98f8897424349d745711dd96fb4b9dc6735d3ff7
Diego <diego.vid.eco@gmail.com>
2019-12-27 23:19:24 -0600

    Updates project clj with empty jvm options

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :jvm-opts ^:replace []
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [overtone "0.10.3"]])
