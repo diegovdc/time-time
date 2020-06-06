ac4489c31083019564670da4fa5730d54fd39986
Diego <diego.vid.eco@gmail.com>
2020-06-06 03:12:50 -0500

    Adds refactor-nrepl

--- profiles.clj
+++ profiles.clj
@@ -1,4 +1,5 @@
-{:dev {:source-paths ["dev"]
+{:repl {:plugins [[refactor-nrepl "2.5.0"]]}
+ :dev {:source-paths ["dev"]
        :dependencies [[quil "2.5.0"]
                       [org.clojure/tools.namespace "0.3.0-alpha4"]
                       [org.clojure/test.check "0.10.0"]]}}
