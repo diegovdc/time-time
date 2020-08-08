5c0a8b6052d2c3d96968755259aa572985ff9347
Diego <diego.vid.eco@gmail.com>
2020-08-08 00:43:06 -0500

    Updates clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.async "1.2.603"]
                  [com.taoensso/timbre "4.10.0"]
                  [overtone "0.10.6"]])
