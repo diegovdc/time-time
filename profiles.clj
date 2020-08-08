3fbd2090d383803315ba2d9bec170cdad181094f
Diego <diego.vid.eco@gmail.com>
2020-08-08 00:41:32 -0500

    Adds profiles.clj to gitignore

--- profiles.clj
+++
@@ -1,5 +0,0 @@
-{:repl {:plugins [#_[refactor-nrepl "2.5.0"]]}
- :dev {:source-paths ["dev"]
-       :dependencies [[quil "2.5.0"]
-                      [org.clojure/tools.namespace "0.3.0-alpha4"]
-                      [org.clojure/test.check "0.10.0"]]}}
