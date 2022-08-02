e67063cfab5e90571a3c2c0d2f44f030164a4bee
Diego Villaseñor <diego.vid.eco@gmail.com>
2022-08-02 08:19:41 -0500

    Modifies deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject time-time "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.videco/time-time "0.1.0-SNAPSHOT"
+  :description "Polytemporal sequencer"
+  :url "https://github.com/diegovdc/time-time"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
